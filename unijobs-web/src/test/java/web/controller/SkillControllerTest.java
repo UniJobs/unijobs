d060365c531a99acaf11616a348ff0dd4597c542
platonv <platonnvlad@gmail.com>
2019-01-13 19:06:20 +0200

    Added kind of smoke test

--- unijobs-web/src/test/java/web/controller/SkillControllerTest.java
+++ unijobs-web/src/test/java/web/controller/SkillControllerTest.java
@@ -1,18 +1,25 @@
 package web.controller;
 
 import core.service.SkillService;
+import core.service.SkillServiceImpl;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import web.config.WebConfig;
 
+import javax.servlet.ServletContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,16 +34,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class SkillControllerTest {
 
-    private MockMvc mvc;
-
     @Autowired
-    private SkillService skillService;
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+//    @Autowired
+//    private SkillServiceImpl skillService;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 
     @Test
     public void getAll() throws Exception {
-        when(skillService.getAll()).thenReturn(new ArrayList());
+//        when(skillService.getAll()).thenReturn(new ArrayList());
 
-        mvc.perform(get("/api/skill/skills")
+        mockMvc.perform(get("/api/skill/skills")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", Matchers.hasSize(0)));
